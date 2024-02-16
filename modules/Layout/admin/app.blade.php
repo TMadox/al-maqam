f9c4cf08a50941404c5c18154684ac069e66629f
Tmadox <mamdohahmed33.ma@gmail.com>
2024-02-16 03:24:52 +0200

    ..

--- modules/Layout/admin/app.blade.php
+++ modules/Layout/admin/app.blade.php
@@ -174,7 +174,7 @@
             <div class="container-fluid">
                 <div class="row">
                     <div class="col-md-6 copy-right" >
-                        {{date('Y')}} &copy; {{__('Booking Core by')}} <a href="{{__('https://www.bookingcore.org')}}" target="_blank">{{__('BookingCore Team')}}</a>
+                        {{date('Y')}} &copy; {{__('AL MAQAAM')}} <a href="{{__('https://www.i-tech.org')}}" target="_blank">{{__('BookingCore Team')}}</a>
                     </div>
                     <div class="col-md-6">
                         <div class="text-md-right footer-links d-none d-sm-block">
